7bbc7567457da9cb774e4d2e52d7c71daef287fd
Kenichiro Kadowaki <kenichiro.kadowaki@e-grant.net>
2021-01-25 15:51:28 +0900

    Add zulu-jdk11 fomula

---
+++ Casks/zulu-jdk11.rb
@@ -0,0 +1,29 @@
+cask "zulu" do
+  if Hardware::CPU.intel?
+    version "11.0.10,11.45.27-ca"
+    sha256 "1c59885634ad438c9b198d31b9dd2aa310922484b3a0ffec603eec1b02eb898e"
+
+    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
+        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+
+    depends_on macos: '>= :high_sierra'
+  else
+    version "11.0.10,11.45.27-ca"
+    sha256 "dd7f76d637fcf692081ae29af8f694d95e82cf30a9aa9ddea26d8e062c65e4f5"
+
+    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_aarch64.dmg",
+        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+
+    depends_on macos: '>= :big_sur'
+  end
+
+  name "Azul Zulu Java Standard Edition Development Kit"
+  homepage "https://www.azul.com/downloads/zulu/zulu-mac/"
+
+  depends_on macos: ">= :sierra"
+
+  pkg "Double-Click to Install Zulu #{version.major}.pkg"
+
+  uninstall pkgutil: "com.azulsystems.zulu.#{version.major}"
+end
+
